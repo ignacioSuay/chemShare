68eac260119c9039a1edd53938d6ac04f870d7d7
Ignacio Suay <ignacio.suay@gmail.com>
2015-11-10 16:25:40 +0000

    add search structure

---
+++ src/main/webapp/scripts/app/search/search.controller.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module('chemshareApp')
+    .controller('SearchController', function ($scope,$http, Compound) {
+        $scope.compounds = [];
+
+        //Principal.identity().then(function(account) {
+        //    $scope.account = account;
+        //    $scope.loadAll();
+        //});
+        //
+        //$scope.loadAll = function() {
+        //    var userId = $scope.account.login;
+        //    $http.post("api/findCompounds", userId).success(function (response) {
+        //        $scope.compounds = response;
+        //    }).error(function(){alert("Error getting my compounds data")});
+        //};
+        //
+        //
+        //$scope.create = function () {
+        //    Compound.save($scope.compound,
+        //        function () {
+        //            $scope.loadAll();
+        //            $('#saveCompoundModal').modal('hide');
+        //            $scope.clear();
+        //        });
+        //};
+        //
+        //$scope.update = function (id) {
+        //    Compound.get({id: id}, function(result) {
+        //        $scope.compound = result;
+        //        $('#saveCompoundModal').modal('show');
+        //    });
+        //};
+        //
+        //$scope.delete = function (id) {
+        //    Compound.get({id: id}, function(result) {
+        //        $scope.compound = result;
+        //        $('#deleteCompoundConfirmation').modal('show');
+        //    });
+        //};
+        //
+        //$scope.confirmDelete = function (id) {
+        //    Compound.delete({id: id},
+        //        function () {
+        //            $scope.loadAll();
+        //            $('#deleteCompoundConfirmation').modal('hide');
+        //            $scope.clear();
+        //        });
+        //};
+        //
+        //$scope.clear = function () {
+        //    $scope.compound = {structure: null, university: null, price: null, unit: null, userId: null, id: null};
+        //};
+    });
---
+++ src/main/webapp/scripts/app/search/search.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('chemshareApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('search', {
+                parent: 'entity',
+                url: '/search',
+                data: {
+                    roles: ['ROLE_USER'],
+                    pageTitle: 'chemshareApp.compound.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/search/search.html',
+                        controller: 'SearchController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('search');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+    });
